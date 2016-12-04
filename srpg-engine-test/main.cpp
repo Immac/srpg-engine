268c5971b731930218996a5ced51f2999d332b47
Miguel Chicas <immac.gm@gmail.com>
2016-12-04 03:05:20 -0600

    Configured sfml to work with the test project

--- srpg-engine-test/main.cpp
+++ srpg-engine-test/main.cpp
@@ -1,12 +1,40 @@
 #include <iostream>
 #include <gameobject.hpp>
-using namespace std;
+#include <SFML/Window.hpp>
 
-int main(int argc, char *argv[])
+using namespace std;
+using GameObject = SrpgEngine::Game::GameObject;
+//int main(int argc, char *argv[])
+//{
+//	cout << "Hello World!" << endl;
+//	auto gameObject = new GameObject();
+//	gameObject->Name = "Hello";
+
+//	cout << gameObject->Name;
+//	return 0;
+//}
+
+#include <SFML/Graphics.hpp>
+
+int main()
 {
-	cout << "Hello World!" << endl;
-	auto gO = new SrpgEngine::Game::GameObject();
-	gO->Name = "Hello";
-	cout << gO->Name;
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Green);
+
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(shape);
+		window.display();
+	}
+
 	return 0;
 }
