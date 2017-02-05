dd15d463475ecf79d56b54934afe0e2ccefdeb4c
Miguel Chicas <immac.gm@gmail.com>
2017-02-04 18:17:12 -0600

    preparing to change lua management method

--- srpg-engine/src/main.cpp
+++ srpg-engine/src/main.cpp
@@ -1,52 +1,10 @@
 #include <lua.hpp>
 #include <iostream>
 #include "luautil.hpp"
-#include "sqlite3.h"
-
-using std::string;
-static int callback(void *NotUsed, int argc, char **argv, char **azColName){
-   int i;
-   for(i=0; i<argc; i++){
-	  printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
-   }
-   printf("\n");
-   return 0;
-}
-
-
+#include "selene.h"
 //using SrpgEngine::Framework::Lua::LuaScript;
 int main(){
-	sqlite3 *db;
-	char *zErrMsg = 0;
-	int  rc;
-	string sql;
 
-	/* Open database */
-	rc = sqlite3_open("test.db", &db);
-	if( rc ){
-	   fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
-	   return(0);
-	}else{
-	   fprintf(stdout, "Opened database successfully\n");
-	}
-
-	/* Create SQL statement */
-	sql = "CREATE TABLE COMPANY("  \
-		  "ID INT PRIMARY KEY     NOT NULL," \
-		  "NAME           TEXT    NOT NULL," \
-		  "AGE            INT     NOT NULL," \
-		  "ADDRESS        CHAR(50)," \
-		  "SALARY         REAL );";
-
-	/* Execute SQL statement */
-	rc = sqlite3_exec(db, sql.c_str(), callback, 0, &zErrMsg);
-	if( rc != SQLITE_OK ){
-	fprintf(stderr, "SQL error: %s\n", zErrMsg);
-	   sqlite3_free(zErrMsg);
-	}else{
-	   fprintf(stdout, "Table created successfully\n");
-	}
-	sqlite3_close(db);
 	return 0;
 //	LuaScript script("srpg-engine.config");
 //	float posX = script.get<float>("player.position.x");
@@ -77,5 +35,5 @@ int main(){
 //			std::cout<<*it<<",";
 //	}
 //	std::cout<<std::endl;
-	return 0;
+//	return 0;
 }
