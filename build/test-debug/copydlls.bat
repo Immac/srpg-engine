6ba303883d3434a68a5e8c217f26bcd7021bdd2d
Miguel Chicas <immac.gm@gmail.com>
2017-05-31 21:05:13 -0600

    added several files that are kind of important

---
+++ build/test-debug/copydlls.bat
@@ -0,0 +1,7 @@
+pushd ..\..\..\install\
+copy srpg-engine\bin\*.dll ..\proyecto-de-graduacion\build\test-debug\ /y
+copy srpg-s2dge-system\bin\*.dll ..\proyecto-de-graduacion\build\test-debug\ /y
+copy lua53\bin\*.dll ..\proyecto-de-graduacion\build\test-debug\ /y
+copy simple-position-system\bin\*.dll ..\proyecto-de-graduacion\build\test-debug\ /y
+copy s2dge-tilepos-adapter\bin\*.dll ..\proyecto-de-graduacion\build\test-debug\ /y
+popd 
